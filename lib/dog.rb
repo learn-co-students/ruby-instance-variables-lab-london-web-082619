2527bc920fd56e573891e5f5a2f6d44a5b50ee95
SarahDrake01 <jussaradrake@outlook.com>
2019-09-16 08:52:54 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,17 @@
 class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
- 
-  def name
-    @this_dogs_name
+def name=(dog_name)
+  @this_dogs_name = dog_name 
+  #name variable doen't know about the this_dogs_name variable from the #name= method because this_dogs_name is a local variable 
+  #A local variable has a local scope. That means that it cannot be accessed outside of the methid in which it is defined
+end
+
+def name
+  @this_dogs_name
   end
 end
- 
+
+
 lassie = Dog.new
 lassie.name = "Lassie"
- 
+
 puts lassie.name
